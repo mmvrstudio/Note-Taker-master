1ff18062db0a103a46a8b57ff7bf039855071a39
Mariana Marques <65464431+mmvrstudio@users.noreply.github.com>
2022-04-26 11:54:51 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+app.use(express.static("public"));
+app.listen(PORT, function() {
+    console.log("We are now live on Port: " + PORT);
+});
